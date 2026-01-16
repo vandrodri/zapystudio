c8ed8d1684aecc6e34ab7030c6ef5f36f560c79e
vandrodri <aprender.van@gmail.com>
2026-01-16 12:31:48 -0300

    usa router.huggingface.co

--- netlify/functions/generate-logo.js
+++ netlify/functions/generate-logo.js
@@ -33,7 +33,7 @@ exports.handler = async (event) => {
     console.log('Chamando HuggingFace API...');
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1",
+      "https://router.huggingface.co/models/stabilityai/stable-diffusion-2-1",
       {
         headers: {
           Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
